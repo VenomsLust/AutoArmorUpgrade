9d5429884c48da2ae5fbff2f6d21be4bcd615b43
crand <crand@MATRIXPRIME>
2026-03-04 18:59:08 -0500

    Add project files.

---
+++ MySubModule.cs
@@ -0,0 +1,20 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+
+namespace AutoArmorUpgrade {
+    public class MySubModule : MBSubModuleBase {
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject) {
+            base.OnGameStart(game, gameStarterObject);
+
+            if (gameStarterObject is CampaignGameStarter campaignGameStarter) {
+                campaignGameStarter.AddBehavior(new AutoArmorBehavior());
+            }
+        }
+
+
+        protected override void OnSubModuleLoad() {
+            base.OnSubModuleLoad();
+        }
+    }
+}
